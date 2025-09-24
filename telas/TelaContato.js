6f5ce1c82d04fbdfc0ef011654829f3ac8a09c20
Lucas Anjos <oargus.g@gmail.com>
2025-09-23 22:18:26 -0300

    Criando telas

---
+++ telas/TelaContato.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function TelaContato() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Bem-vindo à Tela de Contato!</Text>
+        </View>
+    );
+}
+
+export default TelaContato;
---
+++ telas/TelaInicio.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function TelaInicio() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Bem-vindo à Tela de Início!</Text>
+        </View>
+    );
+}
+
+export default TelaInicio;
